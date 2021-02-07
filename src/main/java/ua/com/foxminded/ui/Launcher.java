09d1623b14cef8e487698e80072c70a40fa3600c
VLapinskyi <valentinlapinskiy@gmail.com>
2021-02-07 23:20:02 +0200

    Correcting Launcher

--- src/main/java/ua/com/foxminded/ui/Launcher.java
+++ src/main/java/ua/com/foxminded/ui/Launcher.java
@@ -1,22 +1,5 @@
 package ua.com.foxminded.ui;
 
-import java.time.LocalTime;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.format.datetime.joda.LocalTimeParser;
-
-import ua.com.foxminded.dao.GroupDAO;
-import ua.com.foxminded.dao.LecturerDAO;
-import ua.com.foxminded.dao.LessonTimeDAO;
-import ua.com.foxminded.dao.StudentDAO;
-import ua.com.foxminded.domain.Faculty;
-import ua.com.foxminded.domain.Gender;
-import ua.com.foxminded.domain.Group;
-import ua.com.foxminded.domain.Lecturer;
-import ua.com.foxminded.domain.LessonTime;
-import ua.com.foxminded.domain.Student;
-import ua.com.foxminded.settings.SpringConfiguration;
-
 public class Launcher {
 
     public static void main(String[] args) {
