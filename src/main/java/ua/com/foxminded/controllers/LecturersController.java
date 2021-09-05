d6021f4e570a143209bc8e6af6b702f9bd39f27c
Boy1 <Boy1@DESKTOP-68ER3GD>
2021-09-05 23:12:26 +0300

    Update lecturer

--- src/main/java/ua/com/foxminded/controllers/LecturersController.java
+++ src/main/java/ua/com/foxminded/controllers/LecturersController.java
@@ -1,10 +1,15 @@
 package ua.com.foxminded.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +54,24 @@ public class LecturersController {
     public String createLecturer(@ModelAttribute("lecturer") Lecturer lecturer, @RequestParam("gender-value") String gender) {
         lecturer.setGender(Gender.valueOf(gender));
         lecturerService.create(lecturer);
+        return "redirect:/lecturers";
+    }
+    
+    @GetMapping("/{id}/edit")
+    public String editLecturer(Model model, @PathVariable("id") int id) {
+        Lecturer lecturer = lecturerService.getById(id);
+        List<Gender> genders = new ArrayList<>(Arrays.asList(Gender.values()));
+        genders.remove(lecturer.getGender());
+        model.addAttribute("pageTitle", "Edit " + lecturer.getFirstName() + " " + lecturer.getLastName());
+        model.addAttribute("lecturer", lecturer);
+        model.addAttribute("genders", genders);
+        return "lecturers/edit";
+    }
+    
+    @PatchMapping("/{id}")
+    public String updateLecturer(@ModelAttribute("lecturer") Lecturer lecturer, @RequestParam("gender-value") Gender gender) {
+        lecturer.setGender(gender);
+        lecturerService.update(lecturer);
         return "redirect:/lecturers";
     }
 }
